20dbe463b40b562ae092c47437aa03986b89ce5c
spectreFive <52245535+spectreFive@users.noreply.github.com>
2021-02-12 10:54:16 -0700

    Update entrypoint.sh
    testing out more stuff

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,4 @@
-#!/bin/sh -l
+#!/bin/sh
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
+echo "Hello world my name is $INPUT_MY_NAME"
+echo "This is not very exciting, but I don't know what else to do."
